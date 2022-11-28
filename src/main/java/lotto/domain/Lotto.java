a0ddc9d44b3b70ad27ea4f8005d2569ac035f713
sosow0212 <sosow0212@naver.com>
2022-11-28 22:08:37 +0900

    refactor : (Lotto) 문제에서 틀린 경우 재입력을 받는다고 가정하고 리팩토링 진행

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,22 +1,15 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.exception.LottoException;
 
 public class Lotto {
     private final List<Integer> numbers;
     private final int bonusNumber;
 
     public Lotto(List<Integer> numbers, int bonusNumber) {
-        validate(numbers);
         this.numbers = numbers;
         this.bonusNumber = bonusNumber;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    // TODO: 추가 기능 구현
 }
