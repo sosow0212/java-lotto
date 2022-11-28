7b00879271e09f6fb42b0add10b0dcf18dc54dd3
sosow0212 <sosow0212@naver.com>
2022-11-28 19:31:16 +0900

    feat : (User) 로또 번호를 반환해주는 메서드 추가

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -20,4 +20,8 @@ public class User {
     private int getLottoQuantity() {
         return this.buyingPrice / 1000;
     }
+
+    public List<UserLotto> getLottos() {
+        return lottos;
+    }
 }
