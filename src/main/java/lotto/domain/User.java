a87135e94278bc15cf0faa12d47fefc33967c20c
sosow0212 <sosow0212@naver.com>
2022-11-28 18:50:52 +0900

    feat : (User) 구입 금액과, 구매한 로또를 관리해주는 도메인 생성

--- src/main/java/lotto/domain/User.java
+++ src/main/java/lotto/domain/User.java
@@ -1,14 +1,19 @@
 package lotto.domain;
 
 import java.util.List;
+import lotto.util.LottoGenerator;
 
 public class User {
-    private int buyingPrice;
-    private List<UserLotto> lottos;
+    private final int buyingPrice;
+    private final List<UserLotto> lottos;
 
     public User(int buyingPrice) {
-        // exception 여기서 진행해보기
+        // 예외 처리 진행하기
         this.buyingPrice = buyingPrice;
-        // 추후에 여기서 List<UserLotto> 도 채워야함
+        this.lottos = LottoGenerator.generatorUserLotto(getLottoQuantity());
+    }
+
+    private int getLottoQuantity() {
+        return this.buyingPrice / 1000;
     }
 }
