f2252f6dacc9ee0930a9cc96e8814396a8bf52d8
sosow0212 <sosow0212@naver.com>
2022-11-28 18:56:55 +0900

    refactor : (LottoGenerator) 로또 생성 메서드를 static 메서드로 변경

--- src/main/java/lotto/util/LottoGenerator.java
+++ src/main/java/lotto/util/LottoGenerator.java
@@ -6,8 +6,7 @@ import java.util.List;
 import lotto.domain.UserLotto;
 
 public class LottoGenerator {
-
-    public List<UserLotto> generatorUserLotto(int quantity) {
+    public static List<UserLotto> generatorUserLotto(int quantity) {
         List<UserLotto> userLottos = new ArrayList<>();
         for(int i=0; i<quantity; i++) {
             userLottos.add(new UserLotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
