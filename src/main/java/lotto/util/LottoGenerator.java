d228c56a8108ea23dd641c514315e7cb4934bab8
sosow0212 <sosow0212@naver.com>
2022-11-28 19:18:14 +0900

    refactor : (LottoGenerator) 정렬 후 반환되도록 리팩토링

--- src/main/java/lotto/util/LottoGenerator.java
+++ src/main/java/lotto/util/LottoGenerator.java
@@ -2,15 +2,23 @@ package lotto.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import lotto.domain.UserLotto;
 
 public class LottoGenerator {
     public static List<UserLotto> generatorUserLotto(int quantity) {
         List<UserLotto> userLottos = new ArrayList<>();
-        for(int i=0; i<quantity; i++) {
-            userLottos.add(new UserLotto(Randoms.pickUniqueNumbersInRange(1, 45, 6)));
+        for (int i = 0; i < quantity; i++) {
+            userLottos.add(new UserLotto(createLotto()));
         }
         return userLottos;
+    }
+
+    private static List<Integer> createLotto() {
+        List<Integer> numbers = new ArrayList<>();
+        numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Collections.sort(numbers);
+        return numbers;
     }
 }
