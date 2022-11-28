8f5d4e16d81e896191d8a2bdc6891e9c3ff9e7f6
sosow0212 <sosow0212@naver.com>
2022-11-28 18:29:13 +0900

    feat : (UserLotto) 유저의 로또 번호를 관리하는 도메인 생성 및 로또 번호를 반환해주는 getter 구현

---
+++ src/main/java/lotto/domain/UserLotto.java
@@ -0,0 +1,15 @@
+package lotto.domain;
+
+import java.util.List;
+
+public class UserLotto {
+    private final List<Integer> lotto;
+
+    public UserLotto(List<Integer> lotto) {
+        this.lotto = lotto;
+    }
+
+    public List<Integer> getLotto() {
+        return lotto;
+    }
+}
