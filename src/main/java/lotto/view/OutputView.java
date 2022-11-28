29ba30bedc9e3fdef03d45863608f4ef26266e24
sosow0212 <sosow0212@naver.com>
2022-11-28 19:21:07 +0900

    refactor : (OutputView) 출력 형식을 맞추기 위해 줄바꿈 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -8,8 +8,12 @@ public class OutputView {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
-    public void printBuyingQuantityOfLotto(int quantity) {
-        System.out.println(quantity + "개를 구매했습니다.");
+    public void printBuyingQuantityOfLotto(int buyingPrice) {
+        System.out.println("\n" + getBuyingQuantity(buyingPrice) + "개를 구매했습니다.");
+    }
+
+    private int getBuyingQuantity(int buyingPrice) {
+        return buyingPrice / 1000;
     }
 
     public void printUserLottos(List<UserLotto> userLottos) {
@@ -21,15 +25,15 @@ public class OutputView {
     }
 
     public void printAskCorrectNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println("\n당첨 번호를 입력해 주세요.");
     }
 
     public void printAskBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println("\n보너스 번호를 입력해 주세요.");
     }
 
     public void printResult() {
-        System.out.println("당첨 통계");
+        System.out.println("\n당첨 통계");
         System.out.println("---");
         // 추후에 추가
     }
