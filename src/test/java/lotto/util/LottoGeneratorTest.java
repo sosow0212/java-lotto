649bf27807cd45c6951ff59bd6ffb0a5d85a6235
sosow0212 <sosow0212@naver.com>
2022-11-28 18:38:06 +0900

    test : (LottoGeneratorTest) 로또 번호를 생성해주는 util 테스트

---
+++ src/test/java/lotto/util/LottoGeneratorTest.java
@@ -0,0 +1,22 @@
+package lotto.util;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoGeneratorTest {
+    private final static int LOTTO_SIZE = 6;
+    LottoGenerator lottoGenerator = new LottoGenerator();
+
+    @DisplayName("로또 번호를 생성 테스트")
+    @Test
+    void lottoGeneratorTest() {
+        // when
+        List<Integer> numbers = lottoGenerator.generatorLottoNumber();
+
+        // then
+        assertThat(numbers.size()).isEqualTo(LOTTO_SIZE);
+    }
+}
